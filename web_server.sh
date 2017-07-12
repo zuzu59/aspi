6395e46e969b13bae5b94a375ab19561bde74619
Christian Zufferey <christian@zufferey.com>
2017-07-12 11:40:38 +0200

    - ajouté dans le script web_server.sh, la petite astuce pour faire tourner le serveur WEB dans un screen afin de le garder pérenne

--- web_server.sh
+++ web_server.sh
@@ -2,5 +2,17 @@
 #Petit script pour juste faire un petit serveur web en Python en seulement une ligne commande
 #zf170711.1849
 
-python3 -m http.server 8081
+echo -e " 
+
+Afin de garder le petit serveur WEB permanent, il serait bien de le faire tourner dans un 'screen' avec:
+screen -S aspi        pour entrer dans screen
+./web_server.sh       pour lancer le serveur WEB dans screen
+CTRL+a,d              pour sortir de screen en laissant tourner le serveur
+screen -r aspi        pour revenir dans screen
+CTRL+d                pour terminer screen
+screen -list          pour lister tous les screen en fonctionement
+"
+
+
+#python3 -m http.server 8081
 
